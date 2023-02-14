90bb89333a24b87941f48c2fc07a09a9378c5e9b
shanvarun1996 <varun13119982gmail.com>
2023-02-14 16:47:48 +0530

    first commit

---
+++ shan/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from .models import Home, About, Profile, Category, Skills, Portfolio
+
+def index(request):
+
+    # Home
+    home = Home.objects.latest('updated')
+    # home = Home.objects.all()
+
+    # # About
+    about = About.objects.latest('updated')
+    # # about = About.objects.all()
+    profiles = Profile.objects.filter(about=about)
+
+    # Skills
+    categories = Category.objects.all()
+
+    # Portfolio
+    portfolios = Portfolio.objects.all()
+    
+
+    context = {
+        'home': home,
+        'about': about,
+        'profiles': profiles,
+        'categories': categories,
+        'portfolios': portfolios
+    }
+
+
+    return render(request, 'index.html', context)
